5cdda01f572bf326412767ddcdbe23f026f5a113
albert23 <accuri3@gmail.com>
2022-05-31 16:53:05 +0200

    1

--- src/hooks/useAgencyNews.js
+++ src/hooks/useAgencyNews.js
@@ -5,7 +5,7 @@ const API_KEY = 'ba1a0ea8f8830ecdcbd3eaa304985c08';
 
 const fetchNews = async () => {
     let limit = 100;
-    return await axios.get(`http://api.mediastack.com/v1/news?access_key=${API_KEY}&limit=${limit}&countries=us,gb,de&languages=en&categories=technology`).then(res => res.data);
+    return await axios.get(`https://api.mediastack.com/v1/news?access_key=${API_KEY}&limit=${limit}&countries=us,gb,de&languages=en&categories=technology`).then(res => res.data);
 }
 
 export const useAgencyNews = (agency) => {
